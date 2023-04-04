db9bfe6c806060b4b47535f6190e062b28e1c106
Sergey Savchuk <sleepingdog.dev@gmail.com>
2023-04-04 18:15:30 +0300

    gh actions fix

--- .github/workflows/scripts/install_flutter.sh
+++ .github/workflows/scripts/install_flutter.sh
@@ -4,4 +4,4 @@ git clone https://github.com/flutter/flutter.git --depth 1 -b stable _flutter
 echo "$GITHUB_WORKSPACE/_flutter/bin" >> "$GITHUB_PATH"
 
 cd "$GITHUB_WORKSPACE/_flutter/bin"
-flutter doctor
+./flutter doctor
