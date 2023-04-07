df5439fd38ece37d74b41dc6087e72d30ff817ec
Sergey Savchuk <sleepingdog.dev@gmail.com>
2023-04-07 22:51:37 +0300

    increase android integration tests timeout
    android integration test started to fail due to runner's limited computing resources, increasing the test timeout to get around this

--- example/integration_test/android_test.dart
+++ example/integration_test/android_test.dart
@@ -1,3 +1,5 @@
+@Timeout(Duration(seconds: 45))
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:vosk_flutter/vosk_flutter.dart';
